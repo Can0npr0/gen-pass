27708377d649d8c44214445eed6465b60c196f99
Can0npr0 <31983999+Can0npr0@users.noreply.github.com>
2017-09-15 21:17:08 +0300

    Update genpass.py

--- genpass.py
+++ genpass.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Генератор паролей v 0.21
+# Генератор паролей v 0.22
 
 import random
 import datetime as t
@@ -19,42 +19,48 @@ alpha = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o'],
 
 matr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-num = input('Колличество символов: ')     # Колличество символов в пароле
-colll = input('Колличество паролей: ')    # Колличество самих паролей
+try:
+
+    num = input('Колличество символов: ')     # Колличество символов в пароле
+    colll = input('Колличество паролей: ')    # Колличество самих паролей
 
 
 # Генератор по матрице
-def generator():
-    c = random.choice(matr)
-    b = random.choice(matr)
-    d = alpha[c][b]
-    return d
+    def generator():
+        c = random.choice(matr)
+        b = random.choice(matr)
+        d = alpha[c][b]
+        return d
 
 
 # Генератор по колличеству символов в пароле
-def coll(num):
-    c = ''
-    d = range(num)
-    for x in d:
-        h = generator()
-        c = c.__add__(h)
-    return c
+    def coll(num):
+        c = ''
+        d = range(num)
+        for x in d:
+            h = generator()
+            c = c.__add__(h)
+        return c
 
 
 # Генерация колличества самих паролей и добавление их в список
-def genColl(colll):
-    c = []
-    d = range(colll)
-    for x in d:
-        h = coll(num)
-        c.append(h)
-    return c
+    def genColl(colll):
+        c = []
+        d = range(colll)
+        for x in d:
+            h = coll(num)
+            c.append(h)
+        return c
 
 
 # Пробег по списку сгенерированном функцией genColl() и вывод его на экран
-for x in genColl(colll):
-    ps = x
-    print '| ' + str(ps) + ' | ' + str(t.datetime.today()) + ' |'
-    text.write(ps + '\n')
+    for x in genColl(colll):
+        ps = x
+        print '| ' + str(ps) + ' | ' + str(t.datetime.today()) + ' |'
+        text.write(ps + '\n')
 
-text.close()
+    text.close()
+
+except NameError:
+    print 'Только цифры'
+    print 'Повторите попытку'
